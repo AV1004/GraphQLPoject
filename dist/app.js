a18d750aa5bf40da754931bb631bdaf0a72e4f10
Valay Andhariya <andhariyavalay4@gmail.com>
2024-09-02 13:41:18 +0530

    Create  basic apollo sandbox for testing graphql queries and write a basic query schema and resolver for that

--- dist/app.js
+++ dist/app.js
@@ -2,59 +2,48 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import dotenv from "dotenv";
-
 dotenv.config({ path: "./.env" });
-
 export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
 const port = Number(process.env.PORT) || 3000;
-
 // So first you need to made a server and configure it with 2 main properties of graphQL which is schemas and resolvers ,so you have to give schema (i.e typeDefs) as first agrument and resolvers as second argument.
 // Schema is structure of your data in backend
 // Resolvers is actual thing which returns somthing on call of queries from frontend
 const server = new ApolloServer({
-  typeDefs: `type Query { hello:String }`,
-  resolvers: {
-    Query: {
-      hello: () => "Hello World",
+    typeDefs: `type Query { hello:String }`,
+    resolvers: {
+        Query: {
+            hello: () => "Hello World",
+        },
     },
-  },
 });
-
 // So this standaloneserver is use to start apollo provided UI to test GraphQL queries
 startStandaloneServer(server, {
-  listen: {
-    port,
-  },
+    listen: {
+        port,
+    },
 })
-  .then(() => {
+    .then(() => {
     console.log("Server running on port:" + port);
-  })
-  .catch((err) => {
+})
+    .catch((err) => {
     console.log(err);
-  });
-
+});
 // const app = express();
-
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // app.use(cors({ origin: " * ", credentials: true }));
 // app.use(morgan("dev"));
-
 // app.get("/", (req, res) => {
 //   res.send("Hello, World!");
 // });
-
 // your routes here
-
 // app.get("*", (req, res) => {
 //   res.status(404).json({
 //     success: false,
 //     message: "Page not found",
 //   });
 // });
-
 // app.use(errorMiddleware);
-
 // app.listen(port, () =>
 //   console.log("Server is working on Port:" + port + " in " + envMode + " Mode.")
 // );
