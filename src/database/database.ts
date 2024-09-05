c7833dd439d89ec841118ce44778f009b65e1ea1
Valay Andhariya <andhariyavalay4@gmail.com>
2024-09-06 01:51:12 +0530

    Made some changes

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,11 +1,11 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// export const connectDB = (uri: string) =>
-//   mongoose
-//     .connect(uri, { dbName: "graphQLPractice" })
-//     .then((c) => {
-//       console.log(`Connected with ${c.connection.name}`);
-//     })
-//     .catch((e) => {
-//       console.log(e);
-//     });
+export const connectDB = (uri: string) =>
+  mongoose
+    .connect(uri, { dbName: "graphQLPractice" })
+    .then((c) => {
+      console.log(`Connected with ${c.connection.name}`);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
